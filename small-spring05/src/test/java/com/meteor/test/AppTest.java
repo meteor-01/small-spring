a83c1dd3964c56b830a5eaab6ded4a6c5d621247
li-jun-long <1327564517@qq.com>
2023-06-25 16:13:06 +0800

    第三次提交

---
+++ small-spring05/src/test/java/com/meteor/test/AppTest.java
@@ -0,0 +1,64 @@
+package com.meteor.test;
+
+
+import cn.hutool.core.io.IoUtil;
+import com.meteor.test.bean.UserService;
+import com.meteor.beans.factory.support.DefaultListableBeanFactory;
+import com.meteor.beans.factory.xml.XmlBeanDefinitionReader;
+import com.meteor.core.io.DefaultResourceLoader;
+import com.meteor.core.io.Resource;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private DefaultResourceLoader resourceLoader;
+
+    @Before
+    public void init() {
+        resourceLoader = new DefaultResourceLoader();
+    }
+
+    @Test
+    public void test_classpath() throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_file() throws IOException {
+        Resource resource = resourceLoader.getResource("src/test/resources/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_url() throws IOException {
+        Resource resource = resourceLoader.getResource("https://github.com/fuzhengwei/small-spring/blob/main/important.properties");
+        InputStream inputStream = resource.getInputStream();
+        String content = IoUtil.readUtf8(inputStream);
+        System.out.println(content);
+    }
+
+    @Test
+    public void test_xml() {
+        // 1.初始化 BeanFactory
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        // 2. 读取配置文件&注册 Bean
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        reader.loadBeanDefinitions("classpath:spring.xml");
+        // 3. 获取 Bean 对象调用方法
+        UserService userService = beanFactory.getBean("userService", UserService.class);
+        String result = userService.queryUserInfo();
+        System.out.println("测试结果：" + result);
+    }
+
+}
